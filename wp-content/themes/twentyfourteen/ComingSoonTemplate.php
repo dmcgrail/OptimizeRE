9effd1023233d1e81e0a1a13cce35a4df2968fdc
dmcgrail <dmcgrail04@gmail.com>
2014-07-28 16:42:52 -0400

    July28_3

--- wp-content/themes/twentyfourteen/ComingSoonTemplate.php
+++ wp-content/themes/twentyfourteen/ComingSoonTemplate.php
@@ -2,17 +2,19 @@
 /*
 Template Name: Coming_Soon
 */
+$page_bg_image_url = get_background_image();
 ?>
 
 <style>
-	body.custom-background { background-image: url('<?php get_background_image(); ?>');border:1px solid black;}
+	body.custom-background { background-image: url('<?php echo $page_bg_image_url ?>');}
 </style>
 
 <html>
 	<body>
-		Coming soon.
+		Coming soon 1.
 		
 	</body>
 	
 
 </html>
+
