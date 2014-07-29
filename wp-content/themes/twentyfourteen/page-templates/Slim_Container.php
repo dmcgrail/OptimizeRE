b228744c6b212f540992ee570e601742e6862c89
dmcgrail <dmcgrail04@gmail.com>
2014-07-29 17:20:57 -0400

    commit

--- wp-content/themes/twentyfourteen/page-templates/Slim_Container.php
+++ wp-content/themes/twentyfourteen/page-templates/Slim_Container.php
@@ -1,6 +1,7 @@
 <?php
 /*
 Template Name: Slim Container
+the_content();
 */
 ?>
 
@@ -39,17 +40,7 @@ Template Name: Slim Container
 			<div class="boxCenter" >
 				<div class="boxContent">
 					<div class="entry-content">
-						<?php
-							the_content();
-							wp_link_pages( array(
-								'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-								'after'       => '</div>',
-								'link_before' => '<span>',
-								'link_after'  => '</span>',
-							) );
-				
-							edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
-						?>
+						
 					</div><!-- .entry-content -->	
 				</div>
 			</div>
